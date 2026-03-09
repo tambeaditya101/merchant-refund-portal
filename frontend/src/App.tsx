1ebecb6ec8961892615724ae68247be77244d930
adityatambe-personal <tambeaditya101@gmail.com>
2026-03-09 13:26:41 +0530

    add latest working code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Merchant Refund Portal</h1>
+      <p>Application is under active development.</p>
+    </div>
+  );
+}
+
+export default App;
